f7ffb178b3a14ddc348162a476c76ec862cd3502
minhtri06 <pmtri.tvv@outlook.com>
2023-07-15 19:02:37 +0700

    feat: add email service

--- src/configs/envConfig.js
+++ src/configs/envConfig.js
@@ -22,6 +22,12 @@ const envSchema = Joi.object({
     JWT_SECRET: Joi.string().required(),
     JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().integer().min(1).required(),
     JWT_REFRESH_EXPIRATION_DAYS: Joi.number().integer().min(1).required(),
+
+    SMTP_HOST: Joi.string().required(),
+    SMTP_PORT: Joi.number().integer().required(),
+    SMTP_USERNAME: Joi.string().required(),
+    SMTP_PASSWORD: Joi.string().required(),
+    EMAIL_FROM: Joi.string().required(),
 }).unknown()
 
 const { value: envVars, error } = envSchema.validate(process.env)
@@ -33,19 +39,35 @@ const envConfig = {
     PORT: envVars.PORT,
     NODE_ENV: envVars.NODE_ENV,
     CLIENT_URL: envVars.CLIENT_URL,
+
     DEFAULT_PAGE_LIMIT: envVars.DEFAULT_PAGE_LIMIT,
+
     mongodb: {
         URL: envVars.MONGODB_URL,
         DB_NAME: envVars.DB_NAME,
     },
+
     redis: {
         URL: envVars.REDIS_URL,
         DEFAULT_EXPIRATION: envVars.REDIS_DEFAULT_EXPIRATION,
     },
+
     jwt: {
         SECRET: envVars.JWT_SECRET,
         ACCESS_EXPIRATION_MINUTES: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
         REFRESH_EXPIRATION_DAYS: envVars.JWT_REFRESH_EXPIRATION_DAYS,
+    },
+
+    email: {
+        smtp: {
+            host: envVars.SMTP_HOST,
+            port: envVars.SMTP_PORT,
+            auth: {
+                user: envVars.SMTP_USERNAME,
+                pass: envVars.SMTP_PASSWORD,
+            },
+        },
+        from: envVars.EMAIL_FROM,
     },
 }
 
--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,7 @@ module.exports = {
     authService: require("./auth.service"),
     bookingService: require("./booking.service"),
     divisionService: require("./division.service"),
+    emailService: require("./email.service"),
     meService: require("./me.service"),
     propertyService: require("./property.service"),
     redisService: require("./redis.service"),
