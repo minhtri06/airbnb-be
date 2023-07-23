6be05eaaa8db1bed12e0e5f945a22d9d1dc726d5
minhtri06 <pmtri.tvv@outlook.com>
2023-07-23 12:31:53 +0700

    test deploy

--- src/db/redisClient.js
+++ src/db/redisClient.js
@@ -1,7 +1,7 @@
 const { createClient } = require("redis")
 
 const envConfig = require("../configs/envConfig")
-
+console.log(envConfig.redis.URL)
 const redisClient = createClient(envConfig.redis.URL)
 
 module.exports = redisClient
